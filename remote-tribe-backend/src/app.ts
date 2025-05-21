10811a9729c52dfb6361b8af67df458ee0797642
furqanrupom <furqanrupom978@gmail.com>
2025-05-21 18:42:41 +0600

    added logger in email and global error handler

--- remote-tribe-backend/src/app.ts
+++ remote-tribe-backend/src/app.ts
@@ -7,7 +7,7 @@ import morgan from "morgan"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true
 }))
 app.use(morgan('combined', { stream: morganStream }));
